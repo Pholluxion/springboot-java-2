266c5f50e47f166921aaf6d926c31cd9c16813a4
Carlos Peñaloza <carlos.penaloza@dcsas.com.co>
2023-09-02 18:35:55 -0500

    Aggregate development and production profiles.

--- src/main/java/com/example/springbootjava/DemoApplication.java
+++ src/main/java/com/example/springbootjava/DemoApplication.java
@@ -1,13 +1,21 @@
 package com.example.springbootjava;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Value("${spring.datasource.url}")  // our property for the active profile
+	private String dataByEnvironment;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@PostConstruct
+	public void doSomething() {
+		System.out.println("Property spring.datasource.url value is " + dataByEnvironment);
+	}
 }
